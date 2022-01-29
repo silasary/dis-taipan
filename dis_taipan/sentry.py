95412fbdd9ca405766320350b848455edb5c3ded
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-01-29 15:04:22 +1100

    feat(sentry): Catch and replace `default_error_handler`

--- dis_taipan/sentry.py
+++ dis_taipan/sentry.py
@@ -10,8 +10,11 @@ import os
 def sentry_filter(event: dict[str, Any], hint: dict[str, Any]):  # type: ignore
     if 'log_record' in hint:
         record: logging.LogRecord = hint['log_record']
-        if 'dis.snek' in record.name and '/commands/permissions: 403' in record.message:
-            return None
+        if 'dis.snek' in record.name:
+            if '/commands/permissions: 403' in record.message:
+                return None
+            if record.message.startswith('Ignoring exception in '):
+                return None
 
     if 'exc_info' in hint:
         exc_type, exc_value, tb = hint['exc_info']
@@ -19,14 +22,23 @@ def sentry_filter(event: dict[str, Any], hint: dict[str, Any]):  # type: ignore
             return None
     return event
 
+_default_error_handler = Snake.default_error_handler
+
 class SentryScale(Scale):
     @listen()
     async def on_startup(self) -> None:
         sentry_sdk.set_context('bot', {
-            'name': self.bot.user.name,
+            'name': str(self.bot.user),
             'intents': repr(self.bot.intents),
         })
 
+    def default_error_handler(self, source: str, error: Exception) -> None:
+        with sentry_sdk.configure_scope() as scope:
+            scope.set_tag('source', source)
+            sentry_sdk.capture_exception(error)
+        _default_error_handler(source, error)
+
+    Snake.default_error_handler = default_error_handler
 
 def setup(bot: Snake) -> None:
     token = os.environ.get('SENTRY_TOKEN')
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Silasary",
     url="https://github.com/silasary/dis-taipan",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     package_data={"dis_taipan": ["py.typed", "*.pyi", "**/*.pyi"]},
     
