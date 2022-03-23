38392c44952ada1289cfb051b07922f62eb01595
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-03-23 14:46:23 +1100

    More linting

--- tests/protocols_test.py
+++ tests/protocols_test.py
@@ -2,6 +2,7 @@ from dis_snek import InteractionContext, MessageContext
 from typeguard import check_type
 from dis_taipan import protocols
 
-def test_sendable_context():
+
+def test_sendable_context() -> None:
     check_type('message_context', MessageContext, protocols.SendableContext)
     check_type('interaction_context', InteractionContext, protocols.SendableContext)
