a55d2aecd42fd032e068ef677d2f99fa5e242240
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-03-28 15:23:01 +1100

    chore: bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Silasary",
     url="https://github.com/silasary/dis-taipan",
-    version="0.2.8",
+    version="0.2.9",
     packages=find_packages(exclude='tests'),
     package_data={"dis_taipan": ["py.typed", "*.pyi", "**/*.pyi"]},
     python_requires=">=3.10",
